8b11f14f1c6c1a8290660600c18a9d436d307f86
Lily <lrfaizova@edu.hse.ru>
2025-04-27 23:24:21 +0300

    testtest5

--- collect_files.sh
+++ collect_files.sh
@@ -1,60 +1,40 @@
 #!/bin/bash
 
+funct(){
+    input_dir=$1
+    output_dir=$2
+    for file in $input_dir/*; do
+        if [ -f "$file" ]; then
+            copy $file $output_dir
+        fi
+        if [ -d "$file" ]; then
+            funct $file $output_dir
+        fi
+    done
+}
+
+copy(){
+    file=$1
+    output_dir=$2
+    name="$(basename "$file")"
+    ext="${name##*.}"
+    base="${name%.*}"
+    count=0
+    if [ $ext == $base ]; then
+        ext=""
+    else
+        ext=".$ext"
+    fi
+    while [ -e "$output_dir/$name" ]; do
+        (( count++ ))
+        name="${base}${count}${ext}"
+    done
+    cp "$file" "$output_dir/$name"
+}
+
 input_dir=$1
 output_dir=$2
 
-# Имитация функции funct с помощью стека
-stack=("$input_dir")
 
-while [ ${#stack[@]} -gt 0 ]; do
-    current_dir=${stack[-1]}
-    unset 'stack[${#stack[@]}-1]'
-    
-    for file in "$current_dir"/*; do
-        if [ -f "$file" ]; then
-            # Начало замены функции copy
-            name="$(basename "$file")"
-            ext="${name##*.}"
-            base="${name%.*}"
-            
-            if [ "$ext" == "$base" ]; then
-                ext=""
-            else
-                ext=".$ext"
-            fi
-            
-            # Проверка на существование файлов с таким же именем
-            for output_file in "$output_dir"/*; do
-                output_name="$(basename "$output_file")"
-                if [ "$name" == "$output_name" ]; then
-                    mv -- "$output_file" "$output_dir/${base}1$ext"
-                fi
-            done
-            
-            # Поиск максимального номера
-            ma=0
-            for output_file in "$output_dir"/*; do
-                b=${output_file##*/}
-                num=${b#"$base"}
-                num=${num%"$ext"}
-                if [[ $num =~ ^[0-9]+$ ]]; then
-                    if (( num > ma )); then
-                        ma=$num
-                    fi
-                fi
-            done
-            
-            # Копирование файла с новым именем
-            if (( ma == 0 )); then
-                cp "$file" "$output_dir/${base}$ext"
-            else
-                (( ma++ ))
-                cp "$file" "$output_dir/${base}$ma$ext"
-            fi
-            # Конец замены функции copy
-            
-        elif [ -d "$file" ]; then
-            stack+=("$file")
-        fi
-    done
-done
+
+funct $input_dir $output_dir
