b74705aa69e90b10785e105c8425102895a75880
Lily <lrfaizova@edu.hse.ru>
2025-04-28 21:03:03 +0300

    testtt

--- collect_files.sh
+++ collect_files.sh
@@ -1,39 +1,37 @@
 #!/bin/bash
 
-funct(){
-
-    input_dir=$1
-    output_dir=$2
-    echo $input_dir
-    local -i cur_depth=$3
-    max_depth=$4
+funct() {
+    local input_dir="$1"
+    local output_dir="$2"
+    local -i cur_depth="$3"
+    local max_depth="$4"
     (( cur_depth++ ))
-    for file in $input_dir/*; do
+    for file in "$input_dir"/*; do
         if [ -f "$file" ]; then
-            copy $file $output_dir $cur_depth $max_depth
+            copy "$file" "$output_dir" "$cur_depth" "$max_depth"
         fi
         if [ -d "$file" ]; then
-            funct $file $output_dir $cur_depth $max_depth
+            funct "$file" "$output_dir" "$cur_depth" "$max_depth"
         fi
     done
 }
 
-copy(){
-    file=$1
-    output_dir=$2
-    local -i cur_depth=$3
-    max_depth=$4
-    path="${file#*/}"
-    ext="${path##*.}"
-    base="${path%.*}"
-    count=0
-    if [ $ext == $base ]; then
+copy() {
+    local file="$1"
+    local output_dir="$2"
+    local -i cur_depth="$3"
+    local max_depth="$4"
+    local path="${file#*/}"
+    local ext="${path##*.}"
+    local base="${path%.*}"
+    local count=0
+    if [ "$ext" == "$base" ]; then
         ext=""
     else
         ext=".$ext"
     fi
 
-    if (( $cur_depth<=$max_depth )); then
+    if (( cur_depth <= max_depth )); then
         while [ -e "$output_dir/$path" ]; do
             (( count++ ))
             path="${base}${count}${ext}"
@@ -42,7 +40,7 @@ copy(){
         mkdir -p "$(dirname "$output_dir/$path")"
         cp "$file" "$output_dir/$path"
     else
-        new_path=$path
+        local new_path="$path"
         for (( i=0; i<(cur_depth-max_depth); i++ )); do
             new_path="${new_path#*/}"
         done
@@ -53,13 +51,12 @@ copy(){
             new_path="${base}${count}${ext}"
         done
         mkdir -p "$(dirname "$output_dir/$new_path")"
-        cp $file "$output_dir/$new_path"
+        cp "$file" "$output_dir/$new_path"
     fi
-
 }
 
-input_dir=$1
-output_dir=$2
+input_dir="$1"
+output_dir="$2"
 shift 2
 MAX_DEPTH=1
 
@@ -74,7 +71,6 @@ while [[ $# -gt 0 ]]; do
     *)
       break;;
   esac
-done #именованные флаги https://unix.stackexchange.com/questions/129391/passing-named-arguments-to-shell-scripts
+done
 
-funct $input_dir $output_dir 0 $MAX_DEPTH
-
+funct "$input_dir" "$output_dir" 0 "$MAX_DEPTH"
